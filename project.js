445645f84432e64c8bc5e9a2ab062279f5079c9b
Harshita820 <67039374+Harshita820@users.noreply.github.com>
2023-02-12 16:42:23 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,20 @@
+console.log("hello");
+function newImg(e){
+    e.src = "icons/profile2.png"
+  }
+  function oldImg(e){
+    e.src = "icons/profile.png"
+  }
+  function wImg(e) {
+    e.src= "icons/heart2.png"
+  }
+  function wImg2(e) {
+    e.src = "icons/heart.png"
+  }
+  function sImg(e) {
+    e.src= "icons/shopping-cart2.png"
+  }
+  function sImg2(e) {
+    e.src = "icons/shopping-cart.png"
+  }
+  
